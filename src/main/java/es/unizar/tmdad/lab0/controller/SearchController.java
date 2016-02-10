8593cc1b9ec4fffb1e91df328861721830e4d4b4
mallzgz <612200@unizar.es>
2016-02-10 21:02:10 +0100

    aaaa

--- src/main/java/es/unizar/tmdad/lab0/controller/SearchController.java
+++ src/main/java/es/unizar/tmdad/lab0/controller/SearchController.java
@@ -2,8 +2,6 @@ package es.unizar.tmdad.lab0.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.social.twitter.api.SearchResults;
-//import org.springframework.stereotype.Controller;
-//import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
--- src/main/resources/public/functions.js
+++ src/main/resources/public/functions.js
@@ -1,12 +1,14 @@
-$(function() {
+/*$(function() {
     $.getJSON('https://www.dropbox.com/s/na4c4i01x9zjpkj/data.json?dl=0', function(data) {
+    	console.log("hola");
         var template = $('#autoTPL').html();
         var info = Mustache.to_html(template, data);
          $('#inventarioListado').html(info);
     });
-});
-/*
+});*/
+
 $(document).ready(function() {
+	console.log("hola");
 	registerSearch();
 });
 
@@ -17,4 +19,4 @@ function registerSearch() {
 			$("#resultsBlock").empty().append(data);
 		});	
 	});
-}*/
+}
