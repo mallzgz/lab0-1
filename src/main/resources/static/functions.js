c249c7afdc1209cb40f1a40a30c84a5c17f822e3
mallzgz <612200@unizar.es>
2016-02-11 01:17:29 +0100

    - Subida para comprobación

--- src/main/resources/static/functions.js
+++ src/main/resources/static/functions.js
@@ -1,12 +1,3 @@
-/*$(function() {
-    $.getJSON('https://www.dropbox.com/s/na4c4i01x9zjpkj/data.json?dl=0', function(data) {
-    	console.log("hola");
-        var template = $('#autoTPL').html();
-        var info = Mustache.to_html(template, data);
-         $('#inventarioListado').html(info);
-    });
-});*/
-
 $(document).ready(function() {
 	console.log("hola");
 	registerSearch();
